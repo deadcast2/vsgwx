2aa1ec01d1e6ee6ef5209011ac57040fc54d1569
deadcast <deadcast@sent.com>
2022-10-23 18:48:21 -0500

    Reduced main

--- main.cpp
+++ main.cpp
@@ -17,68 +17,38 @@ private:
 wxDECLARE_EVENT_TABLE();
 };
 
-enum {
-    Minimal_Quit = wxID_EXIT,
-
-    Minimal_About = wxID_ABOUT
-};
-
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-                EVT_MENU(Minimal_Quit, MyFrame::OnQuit)
-                EVT_MENU(Minimal_About, MyFrame::OnAbout)
+                EVT_MENU(wxID_EXIT, MyFrame::OnQuit)
 wxEND_EVENT_TABLE()
 
-wxIMPLEMENT_APP(MyApp);
+wxIMPLEMENT_APP_NO_MAIN(MyApp);
 
 bool MyApp::OnInit() {
     if (!wxApp::OnInit())
         return false;
 
-    MyFrame *frame = new MyFrame("Minimal wxWidgets App");
-
+    MyFrame *frame = new MyFrame("vsgwx");
     frame->Show(true);
 
     return true;
 }
 
-MyFrame::MyFrame(const wxString &title)
-        : wxFrame(NULL, wxID_ANY, title) {
-    // create a menu bar
+MyFrame::MyFrame(const wxString &title) : wxFrame(NULL, wxID_ANY, title) {
     wxMenu *fileMenu = new wxMenu;
+    fileMenu->Append(wxID_EXIT, "E&xit\tAlt-X", "Quit this program");
 
-    // the "About" item should be in the help menu
-    wxMenu *helpMenu = new wxMenu;
-    helpMenu->Append(Minimal_About, "&About\tF1", "Show about dialog");
-
-    fileMenu->Append(Minimal_Quit, "E&xit\tAlt-X", "Quit this program");
-
-    // now append the freshly created menu to the menu bar...
     wxMenuBar *menuBar = new wxMenuBar();
     menuBar->Append(fileMenu, "&File");
-    menuBar->Append(helpMenu, "&Help");
 
-    // ... and attach this menu bar to the frame
     SetMenuBar(menuBar);
-
-    CreateStatusBar(2);
-    SetStatusText("Welcome to wxWidgets!");
 }
 
 void MyFrame::OnQuit(wxCommandEvent & WXUNUSED(event)) {
     Close(true);
 }
 
-void MyFrame::OnAbout(wxCommandEvent & WXUNUSED(event)) {
-    wxMessageBox(wxString::Format
-                         (
-                                 "Welcome to %s!\n"
-                                 "\n"
-                                 "This is the minimal wxWidgets sample\n"
-                                 "running under %s.",
-                                 wxVERSION_STRING,
-                                 wxGetOsDescription()
-                         ),
-                 "About wxWidgets minimal sample",
-                 wxOK | wxICON_INFORMATION,
-                 this);
+int main(int argc, char **argv) {
+    wxEntry(argc, argv);
+
+    return 0;
 }
