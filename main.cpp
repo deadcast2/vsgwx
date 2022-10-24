b0fd56a6ce80dbbad417fede880d2b05f21dfaa6
deadcast <deadcast@sent.com>
2022-10-23 21:47:03 -0500

    OMG it works! Very messy but works.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,119 @@
 #include "wx/wx.h"
+#include <vsg/all.h>
+#include <vsg/viewer/Window.h>
+#include <gtk-3.0/gtk/gtk.h>
+#include <gtk-3.0/gtk/gtkx.h>
+#include <xcb/xcb.h>
+
+class wxViewerWindow : public wxPanel {
+public:
+    wxViewerWindow(wxWindow *parent);
+
+    virtual ~wxViewerWindow();
+
+    void intialize(uint32_t width, uint32_t height);
+
+    void paintEvent(wxPaintEvent &evt);
+
+    void paintNow();
+
+    void render(wxDC &dc);
+
+    vsg::ref_ptr<vsg::WindowTraits> traits;
+    vsg::ref_ptr<vsg::Instance> instance;
+    vsg::ref_ptr<vsg::Viewer> viewer;
+    vsg::ref_ptr<vsg::Window> windowAdapter;
+
+    DECLARE_EVENT_TABLE()
+
+private:
+    bool _initialized = false;
+};
+
+wxViewerWindow::wxViewerWindow(wxWindow *parent) : wxPanel(parent, wxID_ANY) {
+}
+
+wxViewerWindow::~wxViewerWindow() noexcept {}
+
+void wxViewerWindow::intialize(uint32_t width, uint32_t height) {
+    _initialized = true;
+
+    auto widget = GetHandle();
+    gtk_widget_realize(widget);
+
+    auto window = gtk_widget_get_window(widget);
+    auto native = gdk_x11_window_get_xid(window);
+
+    traits = vsg::WindowTraits::create();
+    traits->nativeWindow = static_cast<xcb_window_t>(native);
+    traits->width = width;
+    traits->height = height;
+
+    windowAdapter = vsg::Window::create(traits);
+
+    viewer = vsg::Viewer::create();
+    viewer->addWindow(windowAdapter);
+
+    auto vsg_scene = vsg::read_cast<vsg::Node>("../models/glider.vsgt");
+    if (!vsg_scene) {
+        std::cout << "Failed to load a valid scenene graph, Please specify a 3d "
+                     "model or image file on the command line."
+                  << std::endl;
+        return;
+    }
+
+    vsg::ComputeBounds computeBounds;
+    vsg_scene->accept(computeBounds);
+    vsg::dvec3 centre = (computeBounds.bounds.min + computeBounds.bounds.max) * 0.5;
+    double radius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.6;
+    double nearFarRatio = 0.001;
+    auto lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+    auto perspective = vsg::Perspective::create(
+            30.0,
+            static_cast<double>(width) /
+            static_cast<double>(height),
+            nearFarRatio * radius, radius * 4.5);
+
+    auto camera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(windowAdapter->extent2D()));
+
+    auto commandGraph = vsg::createCommandGraphForView(windowAdapter, camera, vsg_scene);
+    viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+    viewer->compile();
+}
+
+void wxViewerWindow::paintEvent(wxPaintEvent& evt)
+{
+    wxPaintDC dc(this);
+    render(dc);
+}
+
+void wxViewerWindow::paintNow()
+{
+    wxClientDC dc(this);
+    render(dc);
+}
+
+void wxViewerWindow::render(wxDC &dc) {
+    if (viewer->advanceToNextFrame()) {
+        viewer->handleEvents();
+        viewer->update();
+        viewer->recordAndSubmit();
+        viewer->present();
+    }
+}
 
 class MyApp : public wxApp {
 public:
     virtual bool OnInit() wxOVERRIDE;
+
+    void onIdle(wxIdleEvent &evt);
+
+    wxViewerWindow *wxViewer;
+
+    void activateRenderLoop(bool on);
+
+    bool render_loop_on;
 };
 
 class MyFrame : public wxFrame {
@@ -10,8 +121,6 @@ public:
     MyFrame(const wxString &title);
 
     void OnQuit(wxCommandEvent &event);
-
-    void OnAbout(wxCommandEvent &event);
 
 private:
 wxDECLARE_EVENT_TABLE();
@@ -30,8 +139,34 @@ bool MyApp::OnInit() {
     MyFrame *frame = new MyFrame("vsgwx");
     frame->Show(true);
 
+    wxViewer = new wxViewerWindow(frame);
+    wxViewer->intialize(frame->m_width, frame->m_height);
+
+    activateRenderLoop(true);
+
     return true;
 }
+
+void MyApp::activateRenderLoop(bool on) {
+    if (on && !render_loop_on) {
+        Connect(wxID_ANY, wxEVT_IDLE, wxIdleEventHandler(MyApp::onIdle));
+        render_loop_on = true;
+    } else if (!on && render_loop_on) {
+        Disconnect(wxEVT_IDLE, wxIdleEventHandler(MyApp::onIdle));
+        render_loop_on = false;
+    }
+}
+
+void MyApp::onIdle(wxIdleEvent &evt) {
+    if (render_loop_on) {
+        wxViewer->paintNow();
+        evt.RequestMore(); // render continuously, not only once on idle
+    }
+}
+
+BEGIN_EVENT_TABLE(wxViewerWindow, wxPanel)
+                EVT_PAINT(wxViewerWindow::paintEvent)
+END_EVENT_TABLE()
 
 MyFrame::MyFrame(const wxString &title) : wxFrame(NULL, wxID_ANY, title) {
     wxMenu *fileMenu = new wxMenu;
